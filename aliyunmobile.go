a99687ba5833499a27f8f1e001728b550ec4e77c
freedemocracy <65022120+freedemocracy@users.noreply.github.com>
2024-02-04 22:48:43 +0800

    1.0
    1.0 ver.

---
+++ aliyunmobile.go
@@ -0,0 +1,11 @@
+package aliyunmobile
+
+import "math/rand"
+
+// ReadNumber create random number
+func ReadNumber() int {
+	// random number range
+	rnr := 10
+	//  returns, as an int, a non-negative pseudo-random number in [0,n)
+	return rand.Intn(rnr)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/freedemocracy/aliyun_get_mobile
+
+go 1.20
